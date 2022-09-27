dbee2e20b81bd0642aee65f614090c9b978bd731
01xiaoliu <113813882+01xiaoliu@users.noreply.github.com>
2022-09-27 17:13:35 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        int a =300;
+        int b=7;
+        int c=45;
+        System.out.println(b|c);  //0 ture 1 false, |
+        System.out.println(b&c);
+        System.out.println(a>>3);
+        System.out.println(a<<2);
+    }
+}
